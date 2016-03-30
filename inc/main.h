1ea849e9c91bfa59953510a17c171a48e2a79aa2
Philippe Lefevre <philippe1.lefevre@epitech.eu>
2016-03-30 19:58:23 +0200

    [*] Dossier inc

---
+++ inc/main.h
@@ -0,0 +1,36 @@
+/*
+** main.h for TekAdventure in /home/lefevr_h/Repository/Epitech/Year_1/Infographie/gfx_tekadventure/inc
+**
+** Made by Philippe Lefevre
+** Login   <lefevr_h@epitech.net>
+**
+** Started on  Mon Mar 28 20:02:40 2016 Philippe Lefevre
+** Last update Mon Mar 28 20:09:33 2016 Philippe Lefevre
+*/
+
+#ifndef			MAIN_H_
+# define		MAIN_H_
+# include		<lapin.h>
+
+# ifndef		WIN_SETTING
+#  define		WIN_SETTING
+#  define		WIN_NAME	"TEKADVENTURE"
+#  define		WIN_HEIGHT	768
+#  define		WIN_WIDTH	1024
+# endif			/* !WIN_SETTING */
+
+typedef	struct		s_prog
+{
+  t_bunny_window	*win;
+  t_bunny_pixelarray	*array;
+  t_bunny_position	position;
+  t_color		color;
+}			t_prog;
+
+t_bunny_response	mainloop(void *p);
+int			clean(t_prog *prog, int out);
+
+# define		SUCCESS	0
+# define		ERROR	-1
+
+#endif			/* !MAIN_H_ */
