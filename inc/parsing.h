94594c8b45d95d4a132c63625052aeb51265ee9b
Philippe Lefevre <lefevr_h@epitech.net>
2016-04-06 23:15:45 +0200

    [*] parsing file

---
+++ inc/parsing.h
@@ -0,0 +1,16 @@
+/*
+** parsing.h for TekAdventure in /home/lefevr_h/Repository/Epitech/Year_1/Infographie/gfx_tekadventure/inc
+**
+** Made by Philippe Lefevre
+** Login   <lefevr_h@epitech.net>
+**
+** Started on  Wed Apr  6 23:07:19 2016 Philippe Lefevre
+** Last update Wed Apr  6 23:14:53 2016 Philippe Lefevre
+*/
+
+#define			PARSING_H_
+# define		PARSING_H_
+
+int			parsing(const char *file);
+
+#endif			/* !PARSING_H_ */
---
+++ src/parsing/parsing.c
@@ -0,0 +1,16 @@
+/*
+** parsing.c for TekAdventure in /home/lefevr_h/Repository/Epitech/Year_1/Infographie/gfx_tekadventure/src/parsing
+**
+** Made by Philippe Lefevre
+** Login   <lefevr_h@epitech.net>
+**
+** Started on  Wed Apr  6 23:08:59 2016 Philippe Lefevre
+** Last update Wed Apr  6 23:10:28 2016 Philippe Lefevre
+*/
+
+#include		"parsing.h"
+
+int			parsing(const char *file)
+{
+  return (0);
+}
