866524f4992f005a7939be6738a390d54c7e8443
Lefevre Philippe <PhilippeLefevre@users.noreply.github.com>
2016-04-07 02:44:39 +0200

    [*] back.h

---
+++ inc/back.h
@@ -0,0 +1,22 @@
+/*
+** back.h for  in /Users/ethankerdelhue/Documents/Shared/TekAdventure/inc/
+**
+** Made by Ethan Kerdelhue
+** Login   <kerdel_e@epitech.eu>
+**
+** Started on  Wed Apr  6 23:35:23 2016 Ethan Kerdelhue
+** Last update Thu Apr  7 02:40:31 2016 Philippe Lefevre
+*/
+
+#ifndef 		BACK_H_
+# define 		BACK_H_
+
+# include "main.h"
+
+typedef struct 		s_back
+{
+  t_hitbox		*sprite_hitbox;
+  t_texture		*sprite;
+}			t_back;
+
+#endif			/* !BACK_H_ */
