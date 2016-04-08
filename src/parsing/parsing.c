0f08b041b37dedca016205701d64dcbdd2d900e5
Philippe Lefevre <lefevr_h@epitech.eu>
2016-04-09 01:16:10 +0200

    [*] bunny delete ini

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -5,7 +5,7 @@
 ** Login   <lefevr_h@epitech.net>
 **
 ** Started on  Wed Apr  6 23:08:59 2016 Philippe Lefevre
-** Last update Sat Apr  9 00:48:06 2016 Philippe Lefevre
+** Last update Sat Apr  9 01:15:45 2016 Philippe Lefevre
 */
 
 #include		"main.h"
@@ -40,5 +40,6 @@ t_scene			*parsing(const char *file, t_player **player,
     return (NULL);
   if ((scene = load_scene(ini, stockage, ptr_list)) == NULL)
     return (NULL);
+  bunny_delete_ini(ini);
   return (scene);
 }
