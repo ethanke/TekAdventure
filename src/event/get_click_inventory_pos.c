441cc4f3e8e0bee67428ad0bd0f0f4136d7faf77
victor sousa <victor.sousa@epitech.eu>
2016-04-09 13:56:52 +0200

    [-] finis de detecter l'id de la case appuyer

--- src/event/get_click_inventory_pos.c
+++ src/event/get_click_inventory_pos.c
@@ -5,7 +5,7 @@
 ** Login   <sousa_v@epitech.eu>
 **
 ** Started on  Sat Apr  9 11:47:01 2016 Victor Sousa
-** Last update Sat Apr  9 12:55:02 2016 Victor Sousa
+** Last update Sat Apr  9 13:47:45 2016 Victor Sousa
 */
 
 #include		"main.h"
@@ -36,13 +36,14 @@ static int		find_y_place_inv(t_prog *prog,
       click_pos->x <= WIN_WIDTH / 2 +
       (prog->player->hotbar_sprite->width / 2) / 4 - 10 &&
       click_pos->y >= WIN_HEIGHT / 2 -
-      (prog->player->inv_open_sprite->height / 2) / 4 + 10 &&
+      (prog->player->inv_open_sprite->height / 2) / 4 + 12 &&
       click_pos->y <= WIN_HEIGHT / 2 +
-      (prog->player->inv_open_sprite->height / 2) / 4 - 10)
+      (prog->player->inv_open_sprite->height / 2) / 4 - 12)
     {
-      return ((click_pos->x -
+      return ((click_pos->y -
 	       (WIN_HEIGHT / 2 +
-               (prog->player->inv_open_sprite->height / 2) / 4 - 10)) / 40);
+               (prog->player->inv_open_sprite->height / 2) / 4 -
+		10)) / 35 + 4);
     }
   return (-1);
 }
@@ -51,17 +52,17 @@ static int		find_x_place_inv(t_prog *prog,
 					 t_bunny_position *click_pos)
 {
   if (click_pos->x >= WIN_WIDTH / 2 -
-      (prog->player->hotbar_sprite->width / 2) / 4 + 10 &&
+      (prog->player->inv_open_sprite->width / 2) / 4 + 10 &&
       click_pos->x <= WIN_WIDTH / 2 +
-      (prog->player->hotbar_sprite->width / 2) / 4 - 10 &&
+      (prog->player->inv_open_sprite->width / 2) / 4 - 10 &&
       click_pos->y >= WIN_HEIGHT / 2 -
-      (prog->player->inv_open_sprite->height / 2) / 4 + 10 &&
+      (prog->player->inv_open_sprite->height / 2) / 4 + 12 &&
       click_pos->y <= WIN_HEIGHT / 2 +
-      (prog->player->inv_open_sprite->height / 2) / 4 - 10)
+      (prog->player->inv_open_sprite->height / 2) / 4 - 12)
     {
       return ((click_pos->x -
 	       (WIN_WIDTH / 2 - (prog->player->hotbar_sprite->width / 2)
-		/ 4 + 10 )) / 40);
+		/ 4 + 12 )) / 40);
     }
   return (-1);
 }
@@ -74,8 +75,7 @@ int			get_click_place_inventory(t_prog *prog,
 
   x = find_x_place_inv(prog, click_pos);
   y = find_y_place_inv(prog, click_pos);
-  printf("x: %d,  y: %d\n", x, y);
   if (x != -1 && y != -1)
-    return (x + y * 12 + 9);
+    return (x + (y % 4) * 12);
   return (-1);
 }
