41e8e64a307e51a8d964c5a379f4718f3d06cf60
victor sousa <victor.sousa@epitech.eu>
2016-04-09 16:55:13 +0200

    [-] bug fix

--- src/event/event_key.c
+++ src/event/event_key.c
@@ -1,11 +1,11 @@
 /*
 ** event_key.c for tekadventure in /home/sousa_v/rendu/lapin/TekAdventure_temp
-** 
+**
 ** Made by victor sousa
 ** Login   <sousa_v@epitech.net>
-** 
+**
 ** Started on  Wed Apr  6 21:04:44 2016 victor sousa
-** Last update Thu Apr  7 05:29:55 2016 victor sousa
+** Last update Sat Apr  9 16:54:32 2016 Victor Sousa
 */
 
 #include		"main.h"
@@ -24,7 +24,10 @@ t_bunny_response	event_key(t_bunny_event_state state,
       if (prog->player->inventory_open == 0)
 	prog->player->inventory_open = 1;
       else
-	prog->player->inventory_open = 0;
+	{
+	  prog->player->inventory_open = 0;
+	  prog->player->inv_selected = -1;
+	}
     }
   return (GO_ON);
 }
