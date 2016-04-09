2ef80132f87ebd648e46ef45dd8faefae5c45b9a
Philippe Lefevre <lefevr_h@epitech.eu>
2016-04-09 11:05:09 +0200

    [*] t_groun bug fix remplissage

--- src/parsing/parsing_scene.c
+++ src/parsing/parsing_scene.c
@@ -5,7 +5,7 @@
 ** Login   <lefevr_h@epitech.net>
 **
 ** Started on  Thu Apr  7 01:13:52 2016 Philippe Lefevre
-** Last update Sat Apr  9 10:49:58 2016 Philippe Lefevre
+** Last update Sat Apr  9 11:03:43 2016 Philippe Lefevre
 */
 
 #include		"main.h"
@@ -127,10 +127,10 @@ t_ground		*ground_fill(t_bunny_ini *ini, t_scene *scene)
   if ((str = (char *)bunny_ini_get_field(ini, "count", "scene_npc_count", 0)) == NULL)
     return (NULL);
   i = my_getnbr(str);
+  printf("(%d)\n", i);
   j = 0;
-  while (j < i)
+  while (j <= i)
     {
-      npc = scene->npc;
       if ((str = (char *)bunny_ini_get_field(ini, "scene", "npc_id", j)) == NULL)
 	return (my_puterror_g("Error: scene or scene:npc_id not set\n"));
       tab[0] = my_getnbr(str);
@@ -140,25 +140,22 @@ t_ground		*ground_fill(t_bunny_ini *ini, t_scene *scene)
       i = -1;
       while (str[++i] && str[i] != ';');
       tab[2] = my_getnbr(str + i + 1);
-      while (npc->next != NULL)
+      npc = scene->npc;
+      while (npc != NULL)
 	{
 	  if (npc->npc_id == tab[0])
-	    {
-	      scene->ground[(tab[1] + (tab[2] * scene->size.x))].npc = npc;
-	    }
+	    scene->ground[(tab[1] + (tab[2] * scene->size.x))].npc = npc;
 	  npc = npc->next;
 	}
-      printf("(Ici)\n");
       j++;
     }
-  printf("(%d)\n", j);
   if ((str = (char *)bunny_ini_get_field(ini, "count", "scene_decors_count", 0)) == NULL)
     return (NULL);
   i = my_getnbr(str);
+  printf("(%d)\n", i);
   j = 0;
   while (j < i)
     {
-      decors = scene->decors;
       if ((str = (char *)bunny_ini_get_field(ini, "scene", "decors_id", j)) == NULL)
 	return (my_puterror_g("Error: scene or scene:decors_id not set\n"));
       tab[0] = my_getnbr(str);
@@ -168,13 +165,12 @@ t_ground		*ground_fill(t_bunny_ini *ini, t_scene *scene)
       i = -1;
       while (str[++i] && str[i] != ';');
       tab[2] = my_getnbr(str + i + 1);
-      while (decors->next != NULL)
+      decors = scene->decors;
+      while (decors != NULL)
       {
 	if (decors->decors_id == tab[0])
-	    {
-	      scene->ground[(tab[1] + (tab[2] * scene->size.x))].decors = decors;
-	    }
-	decors = decors->next;
+	    scene->ground[(tab[1] + (tab[2] * scene->size.x))].decors = decors;
+	  decors = decors->next;
       }
       j++;
     }
