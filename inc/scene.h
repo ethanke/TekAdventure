2bcb0adacb29865841f1b33306093d7ba227b046
Philippe Lefevre <lefevr_h@epitech.eu>
2016-04-09 09:49:18 +0200

    [*] update scene.h

--- inc/scene.h
+++ inc/scene.h
@@ -5,7 +5,7 @@
 ** Login   <lefevr_h@epitech.net>
 **
 ** Started on  Wed Apr  6 23:07:19 2016 Philippe Lefevre
-** Last update Sat Apr  9 09:48:30 2016 Philippe Lefevre
+** Last update Sat Apr  9 09:49:08 2016 Philippe Lefevre
 */
 
 #ifndef			SCENE_H_
@@ -23,11 +23,11 @@ typedef struct		s_scene
   unsigned int		height;
   t_texture		*sol;
   t_bunny_position	size;
-  t_sprite		*sprite;
-  t_npc			*npc;
-  t_object		*object;
-  t_decors		*decors;
-  t_player		*player;
+  t_sprite		*sprite; /* Ne pas utilser temporaire */
+  t_npc			*npc;	/* Ne pas utilser temporaire */
+  t_object		*object; /* Ne pas utilser temporaire */
+  t_decors		*decors; /* Ne pas utilser temporaire */
+  t_player		*player; /* Ne pas utilser temporaire */
   t_ground		*ground;
   struct t_scene	*prev;
   struct t_scene	*next;
