21479ec5dd79d37dc2e1554fd11a84d6a06edba3
victor sousa <victor.sousa@epitech.eu>
2016-04-15 22:57:26 +0200

    [-] norm inventory

--- src/disp/disp_inventory.c
+++ src/disp/disp_inventory.c
@@ -5,7 +5,7 @@
 ** Login   <sousa_v@epitech.net>
 **
 ** Started on  Thu Apr  7 02:58:27 2016 victor sousa
-** Last update Fri Apr 15 22:47:18 2016 Victor Sousa
+** Last update Fri Apr 15 22:55:52 2016 Victor Sousa
 */
 
 #include		"main.h"
@@ -20,6 +20,26 @@ static void		disp_big_inventory(t_prog *prog, t_bunny_position *pos)
 	      prog->player->inv_open_sprite, prog->pix);
 }
 
+static void		init_stuff(t_prog *prog,
+				   t_font *font, t_bunny_position *pos,
+				   t_bunny_position *_pos)
+{
+  font->font_img = prog->font->font_img;
+  font->font_size = 11;
+  font->font_color.full = 0xFF050505;
+  pos->x = _pos->x + 10;
+  pos->y = _pos->y + 12;
+}
+
+static void 		disp_item_and_quantity(t_prog *prog, t_font font,
+					       int i, t_bunny_position pos)
+{
+  place_image(create_hitbox(pos.x, pos.y, 36, 30),
+	      *prog->player->inventory[i].object->texture_hitbox,
+	      prog->player->inventory[i].object->texture, prog->pix);
+  teknbr(prog->player->inventory[i].amount, &pos, prog->pix, &font);
+}
+
 static void		disp_top_big_inventory(t_prog *prog,
 					       t_bunny_position *_pos,
 					       t_bunny_position *m_pos)
@@ -29,11 +49,7 @@ static void		disp_top_big_inventory(t_prog *prog,
   t_bunny_position	pos;
   t_font		font;
 
-  font.font_img = prog->font->font_img;
-  font.font_size = 11;
-  font.font_color.full = 0xFF050505;
-  pos.x = _pos->x + 10;
-  pos.y = _pos->y + 12;
+  init_stuff(prog, &font, &pos, _pos);
   x = pos.x;
   i = 11;
   while (++i < 48)
@@ -46,10 +62,7 @@ static void		disp_top_big_inventory(t_prog *prog,
       pos.x = (int)x;
       if (prog->player->inventory[i].id != -1)
 	{
-	  place_image(create_hitbox(pos.x, pos.y, 36, 30),
-		      *prog->player->inventory[i].object->texture_hitbox,
-		      prog->player->inventory[i].object->texture, prog->pix);
-	  teknbr(prog->player->inventory[i].amount, &pos, prog->pix, &font);
+	  disp_item_and_quantity(prog, font, i, pos);
 	  if (m_pos->x >= pos.x && m_pos->x <= pos.x + 40.45 &&
 	      m_pos->y >= pos.y && m_pos->y <= pos.y + 36)
 	    disp_item_info(prog, &prog->player->inventory[i], &pos);
