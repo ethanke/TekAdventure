46b6f13529e169577c764071406eb3f9a7d02f7c
kerdel_e <kerdel_e@epitech.eu>
2016-04-15 17:06:11 -0400

    [-] norm fix utils

--- src/utils/my_puterror.c
+++ src/utils/my_puterror.c
@@ -5,13 +5,12 @@
 ** Login   <kerdel_e@epitech.eu>
 **
 ** Started on  Thu Apr  7 01:18:01 2016 Ethan Kerdelhue
-** Last update Fri Apr  8 06:49:41 2016 Ethan Kerdelhue
+** Last update Fri Apr 15 23:02:32 2016 Ethan Kerdelhue
 */
 
 #include 	"main.h"
 
 int		my_puterror(char *str)
 {
-  write(2, str, my_strlen(str));
-  return (0);
+  return (write(2, str, my_strlen(str)));
 }
