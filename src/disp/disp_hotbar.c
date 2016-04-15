fbd240fc1ed7be77e7725218fad4ee46da62cb40
victor sousa <victor.sousa@epitech.eu>
2016-04-15 21:58:54 +0200

    [-] norm disp_hotbar.c

--- src/disp/disp_hotbar.c
+++ src/disp/disp_hotbar.c
@@ -5,14 +5,34 @@
 ** Login   <sousa_v@epitech.eu>
 **
 ** Started on  Sat Apr  9 08:30:39 2016 Victor Sousa
-** Last update Fri Apr 15 03:43:47 2016 Gaëtan Léandre
+** Last update Fri Apr 15 21:58:16 2016 Victor Sousa
 */
 
 #include	 	"main.h"
 
+static void 		disp_item_and_quantity(t_prog *prog,
+					       t_bunny_position pos,
+					       t_bunny_position *m_pos,
+					       int i)
+{
+  t_font		font;
+
+  font.font_img = prog->font->font_img;
+  font.font_size = 11;
+  font.font_color.full = 0xFF050505;
+  place_image(create_hitbox(pos.x, pos.y, 36, 30),
+	      *prog->player->inventory[i].object->texture_hitbox,
+	      prog->player->inventory[i].object->texture, prog->pix);
+  teknbr(prog->player->inventory[i].amount, &pos, prog->pix, &font);
+  if (m_pos->x >= pos.x && m_pos->x <= pos.x + 40.45 &&
+      m_pos->y >= pos.y && m_pos->y <= pos.y + 36)
+    disp_item_info(prog, &prog->player->inventory[i], &pos);
+}
+
 static void		disp_hotbar_sprite(t_prog *prog, t_bunny_position *pos)
 {
-  place_image(create_hitbox(pos->x, pos->y, prog->player->hotbar_sprite->width / 4,
+  place_image(create_hitbox(pos->x, pos->y,
+			    prog->player->hotbar_sprite->width / 4,
 			    prog->player->hotbar_sprite->height / 4),
 	      create_hitbox(0, 0, prog->player->hotbar_sprite->width,
 			    prog->player->hotbar_sprite->height),
@@ -24,11 +44,7 @@ void			disp_hotbar(t_prog *prog, t_bunny_position *m_pos)
   float			x;
   t_bunny_position	pos;
   int			i;
-  t_font		font;
 
-  font.font_img = prog->font->font_img;
-  font.font_size = 11;
-  font.font_color.full = 0xFF050505;
   pos.x = WIN_WIDTH / 2 - (prog->player->hotbar_sprite->width / 2) / 4;
   pos.y = WIN_HEIGHT - prog->player->hotbar_sprite->height / 4;
   disp_hotbar_sprite(prog, &pos);
@@ -40,15 +56,7 @@ void			disp_hotbar(t_prog *prog, t_bunny_position *m_pos)
     {
       pos.x = (int)x;
       if (prog->player->inventory[i].id != -1)
-	{
-	  place_image(create_hitbox(pos.x, pos.y, 36, 30),
-		      *prog->player->inventory[i].object->texture_hitbox,
-		      prog->player->inventory[i].object->texture, prog->pix);
-	  teknbr(prog->player->inventory[i].amount, &pos, prog->pix, &font);
-	  if (m_pos->x >= pos.x && m_pos->x <= pos.x + 40.45 &&
-	      m_pos->y >= pos.y && m_pos->y <= pos.y + 36)
-	    disp_item_info(prog, &prog->player->inventory[i], &pos);
-	}
+	disp_item_and_quantity(prog, pos, m_pos, i);
       x += 40.45;
     }
   disp_selected_hotbar_inv_item(prog);
