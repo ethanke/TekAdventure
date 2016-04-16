cebacc8bf698f0a09e826dfe0470c55899458b61
victor sousa <victor.sousa@epitech.eu>
2016-04-16 06:58:14 +0200

    [-] menu incoming

--- src/disp/disp_menu.c
+++ src/disp/disp_menu.c
@@ -5,7 +5,7 @@
 ** Login   <sousa_v@epitech.eu>
 **
 ** Started on  Sat Apr 16 06:31:23 2016 Victor Sousa
-** Last update Sat Apr 16 06:49:04 2016 Victor Sousa
+** Last update Sat Apr 16 06:57:36 2016 Victor Sousa
 */
 
 #include		"main.h"
@@ -13,21 +13,26 @@
 void			disp_menu(t_prog *prog)
 {
   place_image(create_hitbox(prog->blit_pos.x, prog->blit_pos.y,
-			    prog->menu_bg->width, prog->menu_bg->height),
-	      create_hitbox(prog->menu_bg_pos.x, prog->menu_bg_pos.y,
-			    prog->exchange_sprite->width - 100,
-			    prog->exchange_sprite->height),
-	      prog->exchange_sprite, prog->pix);
+      			    prog->menu_bg->width, prog->menu_bg->height),
+      	      create_hitbox(prog->menu_bg_pos.x, prog->menu_bg_pos.y,
+      			    prog->menu_bg->width - 150,
+      			    prog->menu_bg->height - 100),
+      	      prog->menu_bg, prog->pix);
+  place_image(create_hitbox(0, 0, prog->menu_text->width,
+			    prog->menu_text->height),
+	      create_hitbox(0, 0, prog->menu_text->width,
+			    prog->menu_text->height),
+	      prog->menu_text, prog->pix);
   if (prog->menu_dir == 0)
     {
       prog->menu_bg_pos.x++;
-      if (prog->menu_bg_pos.x >= 100)
+      if (prog->menu_bg_pos.x >= 150)
 	prog->menu_dir = 1;
     }
   else
     {
       prog->menu_bg_pos.x--;
-      if (prog->menu_bg_pos.x >= 0)
+      if (prog->menu_bg_pos.x <= 0)
 	prog->menu_dir = 0;
     }
 }
