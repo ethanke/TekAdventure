0ee20b1997fc098bf0832050f24908ac3baf2073
Philippe Lefevre <philippe1.lefevre@epitech.eu>
2016-04-16 10:35:01 +0200

    push

--- src/parsing/parsing_linkage.c
+++ src/parsing/parsing_linkage.c
@@ -5,7 +5,7 @@
 ** Login   <lefevr_h@epitech.net>
 **
 ** Started on  Thu Apr 14 02:50:32 2016 Philippe Lefevre
-** Last update Fri Apr 15 04:04:02 2016 Philippe Lefevre
+** Last update Sat Apr 16 10:32:39 2016 Philippe Lefevre
 */
 
 #include		"main.h"
@@ -22,7 +22,16 @@ t_object		*link_object(t_object *object, t_sprite *sprite)
       while (tmp_sprite != NULL)
 	{
 	  if (tmp_sprite->id == tmp_object->sprite_id)
-	    tmp_object->texture = tmp_sprite->sprite;
+	    {
+	      tmp_object->texture = tmp_sprite->sprite;
+	      if ((tmp_object->texture_hitbox->x + tmp_object->texture_hitbox->
+		   width > tmp_object->texture->width)
+		  || (tmp_object->texture_hitbox->y + tmp_object->
+		     texture_hitbox->height > tmp_object->texture->height))
+		return (my_puterror_object("Error: hitbox form object id ",
+					   tmp_object->object_id,
+					   " is out of range\n"));
+	    }
 	  tmp_sprite = tmp_sprite->next;
 	}
       tmp_object = tmp_object->next;
@@ -42,7 +51,16 @@ t_decors		*link_decors(t_decors *decors, t_sprite *sprite)
       while (tmp_sprite != NULL)
 	{
 	  if (tmp_sprite->id == tmp_decors->sprite_id)
-	    tmp_decors->texture = tmp_sprite->sprite;
+	    {
+	      tmp_decors->texture = tmp_sprite->sprite;
+	      if ((tmp_decors->texture_hitbox->x + tmp_decors->
+		   texture_hitbox->width > tmp_decors->texture->width)
+		  || (tmp_decors->texture_hitbox->y + tmp_decors->
+		      texture_hitbox->height > tmp_decors->texture->height))
+		return (my_puterror_decors("Error: hitbox form decors id ",
+					   tmp_decors->decors_id,
+					   " is out of range\n"));
+	    }
 	  tmp_sprite = tmp_sprite->next;
 	}
       tmp_decors = tmp_decors->next;
@@ -64,7 +82,16 @@ t_npc			*link_npc(t_npc *npc, t_sprite *sprite,
       while (tmp_sprite != NULL)
 	{
 	  if (tmp_sprite->id == tmp_npc->sprite_id)
-	    tmp_npc->texture = tmp_sprite->sprite;
+	    {
+	      tmp_npc->texture = tmp_sprite->sprite;
+              if ((tmp_npc->texture_hitbox->x + tmp_npc->
+                   texture_hitbox->width > tmp_npc->texture->width)
+		  || (tmp_npc->texture_hitbox->y + tmp_npc->
+		      texture_hitbox->height > tmp_npc->texture->height))
+		return (my_puterror_npc("Error: hitbox form decors id ",
+					   tmp_npc->npc_id,
+					   " is out of range\n"));
+	    }
 	  tmp_sprite = tmp_sprite->next;
 	}
       tmp_object = object;
