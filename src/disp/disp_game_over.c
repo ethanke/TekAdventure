7e65b064ac6cbbba3128a8acaf232196a1c3e216
kerdel_e <kerdel_e@epitech.eu>
2016-04-15 23:22:07 -0400

    [-] fix game over

--- src/disp/disp_game_over.c
+++ src/disp/disp_game_over.c
@@ -5,7 +5,7 @@
 ** Login   <kerdel_e@epitech.eu>
 **
 ** Started on  Sat Apr 16 03:53:37 2016 Ethan Kerdelhue
-** Last update Sat Apr 16 04:36:41 2016 Ethan Kerdelhue
+** Last update Sat Apr 16 05:20:45 2016 Ethan Kerdelhue
 */
 
 #include 		"main.h"
@@ -21,22 +21,27 @@ void			disp_game_over(t_prog *prog)
   pos.x = 0;
   pos.y = 0;
   put_image(prog->game_over_img, prog->pix, &pos);
-  quit_pos.x = 20;
-  quit_pos.y = WIN_HEIGHT - prog->over_quit->height + 10;
-  quit_pos.width = prog->over_quit->width;
-  quit_pos.height = prog->over_quit->height;
-  continu_pos.x = 20;
-  continu_pos.y = WIN_HEIGHT - prog->over_continue->height + 10;
-  continu_pos.width = prog->over_continue->width;
-  continu_pos.height = prog->over_continue->height;
-  quit_fetch.x = 0;
-  quit_fetch.y = 0;
-  quit_fetch.width = prog->over_quit->width;
-  quit_fetch.height = prog->over_quit->height;
-  continu_fetch.x = 0;
-  continu_fetch.y = 0;
-  continu_fetch.width = prog->over_continue->width;
-  continu_fetch.height = prog->over_continue->height;
+  quit_pos = create_hitbox(20, WIN_HEIGHT - (prog->over_quit->height + 10),
+			   prog->over_quit->width,
+			   prog->over_quit->height);
+  quit_fetch = create_hitbox(0, 0, prog->over_quit->width,
+			   prog->over_quit->height);
+  continu_pos = create_hitbox((WIN_WIDTH - 20) - (prog->over_continue->width),
+			      WIN_HEIGHT - (prog->over_continue->height + 10),
+			      prog->over_continue->width,
+			      prog->over_continue->height);
+  continu_fetch = create_hitbox(0, 0, prog->over_continue->width,
+			      prog->over_continue->height);
   place_image(continu_pos, continu_fetch, prog->over_continue, prog->pix);
   place_image(quit_pos, quit_fetch, prog->over_quit, prog->pix);
+ /* if (in_hitbox(&quit_pos, ) == 1)
+    {
+      printf("mdr on quitte");
+    }
+  if (in_hitbox(&continu_pos, ) == 1)
+    {
+      prog->player->life = 100;
+      prog->state = STATE_GAME;
+    }
+  */
 }
