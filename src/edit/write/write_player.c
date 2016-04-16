3ad77cd97affb25b5e295e5a1cfa486211806f42
leandr_g <gaetan.leandre@epitech.eu>
2016-04-16 08:41:05 +0200

    [*]write player

--- src/edit/write/write_player.c
+++ src/edit/write/write_player.c
@@ -5,10 +5,82 @@
 ** Login   <leandr_g@epitech.eu>
 **
 ** Started on  Sat Apr 16 08:11:18 2016 Gaëtan Léandre
-** Last update Sat Apr 16 08:22:58 2016 Gaëtan Léandre
+** Last update Sat Apr 16 08:40:44 2016 Gaëtan Léandre
 */
 
 #include 		"main.h"
+
+int			write_player_amount(t_player *player, int fd, int size)
+{
+  int			i;
+  int			pos;
+
+  i = 0;
+  pos = 0;
+  my_printf(fd, "player_inventory_amount=");
+  i = 0;
+  pos = 0;
+  while (i < SIZE_INVENTORY + 4)
+    {
+      if (player->inventory[i].id != -1)
+	{
+	  if (pos != size - 1)
+	    my_printf(fd, "\"%d\", ", player->inventory[i].amount);
+	  else
+	    my_printf(fd, "\"%d\"\n", player->inventory[i].amount);
+	  pos++;
+	}
+      i++;
+    }
+}
+
+int			write_player_item(t_player *player, int fd, int size)
+{
+  int			i;
+  int			pos;
+
+  i = 0;
+  pos = 0;
+  my_printf(fd, "player_inventory_item=");
+  i = 0;
+  pos = 0;
+  while (i < SIZE_INVENTORY + 4)
+    {
+      if (player->inventory[i].id != -1)
+	{
+	  if (pos != size - 1)
+	    my_printf(fd, "\"%d\", ", player->inventory[i].id);
+	  else
+	    my_printf(fd, "\"%d\"\n", player->inventory[i].id);
+	  pos++;
+	}
+      i++;
+    }
+}
+
+int			write_player_slot(t_player *player, int fd, int size)
+{
+  int			i;
+  int			pos;
+
+  i = 0;
+  pos = 0;
+  my_printf(fd, "player_inventory_slot=");
+  i = 0;
+  pos = 0;
+  while (i < SIZE_INVENTORY + 4)
+    {
+      if (player->inventory[i].id != -1)
+	{
+	  if (pos != size - 1)
+	    my_printf(fd, "\"%d\", ", i);
+	  else
+	    my_printf(fd, "\"%d\"\n", i);
+	  pos++;
+	}
+      i++;
+    }
+}
 
 int			write_player(t_player *player, int fd)
 {
@@ -16,13 +88,18 @@ int			write_player(t_player *player, int fd)
   int			size;
   my_printf(fd, "[player]\nplayer_name=\"%s\"\n", player->name);
   my_printf(fd, "player_life=\"%d\"\n", player->life);
-  my_printf(fd, "player_inventory_count=\"%d\"\n", player->name);
   i = 0;
+  size = 0;
   while (i < SIZE_INVENTORY + 4)
     {
-      
+      if (player->inventory[i].id != -1)
+	size++;
       i++;
     }
-
+  my_printf(fd, "player_inventory_count=\"%d\"\n", player->name);
+  write_player_slot(player, fd, i);
+  write_player_item(player, fd, i);
+  write_player_amount(player, fd, i);
+  my_printf(fd, "player_sprite_id=\"%d\"\n", player->sprite_id);
   my_printf(fd, "\n\n");
 }
