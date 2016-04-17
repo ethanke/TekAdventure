865b6fd7b0ab90fc873d89302933b0b2bcc73669
leandr_g <gaetan.leandre@epitech.eu>
2016-04-17 07:36:49 +0200

    [-]deplacements mieux

--- src/event/disp_move.c
+++ src/event/disp_move.c
@@ -5,20 +5,20 @@
 ** Login   <leandr_g@epitech.eu>
 **
 ** Started on  Sun Apr 17 02:40:55 2016 Gaëtan Léandre
-** Last update Sun Apr 17 02:43:38 2016 Gaëtan Léandre
+** Last update Sun Apr 17 07:36:02 2016 Gaëtan Léandre
 */
 
 #include		"main.h"
 
 t_anim			*chose_deplacement(t_player *player)
 {
-  if (ABS(player->x_vec) > ABS(player->y_vec) && player->x_vec < 0)
+  if (ABS(player->x_vec) >= ABS(player->y_vec) && player->x_vec < 0)
     return (&player->left);
-  else if (ABS(player->x_vec) > ABS(player->y_vec) && player->x_vec > 0)
+  else if (ABS(player->x_vec) >= ABS(player->y_vec) && player->x_vec > 0)
     return (&player->right);
-  else if (ABS(player->x_vec) < ABS(player->y_vec) && player->y_vec < 0)
+  else if (ABS(player->x_vec) <= ABS(player->y_vec) && player->y_vec < 0)
     return (&player->up);
-  else if (ABS(player->x_vec) < ABS(player->y_vec) && player->y_vec > 0)
+  else if (ABS(player->x_vec) <= ABS(player->y_vec) && player->y_vec > 0)
     return (&player->down);
   else if (player->x_vec == 0 && player->y_vec == 0)
     return (&player->stop);
